e22cd619e41b63aa1abec8ee689d4fec045d7368
Jenna Leopold <jleopold424@gmail.com>
2022-05-06 13:55:35 -0400

    create CreateWorkoutForm component

---
+++ src/components/CreateWorkoutForm.jsx
@@ -0,0 +1,34 @@
+const CreateWorkoutForm = ({ newWorkout, handleInputChange, createWorkout }) => {
+  return (
+    <div className="form-container">
+      <h2>Design a Custom Workout Routine</h2>
+      <form onSubmit={createWorkout}>
+        <div className="form-field">
+          <label><b>Title</b>:</label>
+          <input name="title" type="text" onChange={handleInputChange} placeholder="Enter title..." value={newWorkout.title} required />
+        </div>
+        <div className="form-field">
+          <label><b>Training Type</b>:</label>
+          <input name="trainingType" type="text" onChange={handleInputChange} placeholder="Enter training type..." value={newWorkout.trainingType} required />
+        </div>
+        <div className="form-field">
+          <label><b>Target Area</b>:</label>
+          <input name="targetArea" type="text" onChange={handleInputChange} placeholder="Enter target area..." value={newWorkout.targetArea} required />
+        </div>
+        <div className="form-field">
+          <label><b>Duration</b>:</label>
+          <input name="duration" type="text" onChange={handleInputChange} placeholder="Enter duration (in minutes)..." value={newWorkout.duration} required />
+        </div>
+        <div className="form-field">
+          <label><b>Image</b>:</label>
+          <input name="image" type="text" onChange={handleInputChange} placeholder="Enter image path..." value={newWorkout.image} required />
+        </div>
+        <div className="button-wrapper">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default CreateWorkoutForm
