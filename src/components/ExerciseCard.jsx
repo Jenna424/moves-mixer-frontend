9a2535c96aff17de7123f1e3bcd6723dacd1abfe
Jenna Leopold <jleopold424@gmail.com>
2022-05-09 14:34:35 -0400

    add Link in ExerciseCard component

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 const ExerciseCard = ({ _id, name, image, sets, reps, completed, updateExercise, deleteExercise }) => {
   return (
     <div className="exercise-card">
-      <h3>{name}</h3>
+      <Link to={`/exercises/${_id}`}>
+        <h3>{name}</h3>
+      </Link>
       <img src={image} alt={name} />
       <p className="exercise-details"><b>Sets</b>: {sets}</p>
       <p className="exercise-details"><b>Reps</b>: {reps}</p>
