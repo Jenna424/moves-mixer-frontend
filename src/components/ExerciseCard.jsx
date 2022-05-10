0b8a9e9a35d57992f006566a3f837890624a05a5
Jenna Leopold <jleopold424@gmail.com>
2022-05-10 11:27:59 -0400

    fix update exercise button

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -9,8 +9,16 @@ const ExerciseCard = ({ _id, name, image, sets, reps, completed, updateExercise,
       <img src={image} alt={name} />
       <p className="exercise-details"><b>Sets</b>: {sets}</p>
       <p className="exercise-details"><b>Reps</b>: {reps}</p>
-      <button className="update-exercise-btn" disabled={completed === true} onClick={() => updateExercise(_id)}>Mark Complete</button>
-      <button className="delete-exercise-btn" onClick={() => deleteExercise(_id)}>Remove Exercise</button>
+      <div className="exercise-btns-container">
+        <button
+          className="update-exercise-btn"
+          disabled={completed}
+          onClick={() => updateExercise(_id)}
+        >
+          {completed ? 'Completed!': 'Mark Complete'}
+        </button>
+        <button className="delete-exercise-btn" onClick={() => deleteExercise(_id)}>Remove Exercise</button>
+      </div>
     </div>
   )
 }
