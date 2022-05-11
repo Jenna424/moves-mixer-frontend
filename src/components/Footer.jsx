c5f5240f3ecb7c2ba708e885cb26e6e898a111aa
Jenna Leopold <jleopold424@gmail.com>
2022-05-11 10:10:07 -0400

    created Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="footer-design">
+      <div className="dance-container">
+        <img className ="ballerinaOne" src="https://i.imgur.com/DuKBJhV.png" alt="ballerinaOne"/>
+      </div>
+      <hr className="ground-hr" />
+    </div>
+  )
+}
+
+export default Footer
