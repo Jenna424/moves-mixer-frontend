0c5d6296c7bbb5ba6179da2dc6c81c62d5deddd1
Jenna Leopold <jleopold424@gmail.com>
2022-05-11 09:35:42 -0400

    change About page image; use <ul> and <li> instead of <p>

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,29 +7,31 @@ const About = () => {
       </h4>
       <div className="about-page-body">
         <img
-          className="meditation-image"
-          src="https://i.imgur.com/tVxElpS.png"
-          alt="meditative robot"
+          className="yoga-image"
+          src="https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2120&q=80"
+          alt="sunset yoga scene"
         />
         <div className="instructions-wrapper">
           <p className="instructions-intro-paragraph">
             Please follow the instructions below:
           </p>
-          <p>Access information about each workout routine on the homepage.</p>
-          <p>
-            Select an individual routine to view its exercise movements and
-            equipment requirements.
-          </p>
-          <p>
-            Add a new exercise or piece of equipment to the workout on form
-            submission.
-          </p>
-          <p>
-            <p>
+          <ul className="instructions">
+            <li>
+              Access information about each workout routine on the homepage.
+            </li>
+            <li>
+              Select an individual routine to view its exercise movements and
+              equipment requirements.
+            </li>
+            <li>
+              Add a new exercise or piece of equipment to the workout on form
+              submission.
+            </li>
+            <li>
               Mark an exercise as complete as you proceed through the workout.
-            </p>
-          </p>
-          <p>Remove an exercise from the workout.</p>
+            </li>
+            <li>Remove an exercise from the workout.</li>
+          </ul>
         </div>
       </div>
     </div>
