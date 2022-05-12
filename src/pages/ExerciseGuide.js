b21dd2bcafdf4cff36d80b937b820802a8379d65
Jenna Leopold <jleopold424@gmail.com>
2022-05-12 09:37:42 -0400

    refactor ExerciseGuide page

--- src/pages/ExerciseGuide.js
+++ src/pages/ExerciseGuide.js
@@ -17,12 +17,19 @@ const ExerciseInstructions = () => {
   }, [])
 
   return (
-    <div className="exercise-instructions-page">
-      <h3>{exercise.name} Exercise Guide</h3>
-      <img src={exercise.image} alt={exercise.name} />
-      <p>
-        <b>Instructions</b>: <em>{exercise.instructions}</em>
-      </p>
+    <div className="exercise-guide-page">
+      <h1 className="exercise-guide-heading">Exercise Guide</h1>
+      <h3 className="exercise-guide-subheading">
+        Learn the proper technique for:
+      </h3>
+      <h2>{exercise.name}</h2>
+      <img
+        className="exercise-guide-img"
+        src={exercise.image}
+        alt={exercise.name}
+      />
+      <h3 className="instructions-heading">Instructions:</h3>
+      <p className="instructions-p">{exercise.instructions}</p>
     </div>
   )
 }
